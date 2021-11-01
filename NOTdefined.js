d3d6087413b081b735c7ccfdcc3c9f43a746d989
aryamannitp <90312747+aryamannitp@users.noreply.github.com>
2021-11-01 23:13:15 +0530

    Add files via upload

---
+++ NOTdefined.js
@@ -0,0 +1,12 @@
+   
+if(x==undefined)  //NOTdefined variabe
+console.log("variable is undefined");
+x=10;
+if(x==undefined)
+{
+console.log("variable is undeined");
+}
+else
+{
+    console.log("variable is defined");
+}
---
+++ undefined.js
@@ -0,0 +1,12 @@
+var x;      // global variable 
+if(x==undefined) 
+console.log("variable is undefined");
+x=10;
+if(x==undefined)
+{
+console.log("variable is undeined");
+}
+else
+{
+    console.log("variable is defined");
+}
